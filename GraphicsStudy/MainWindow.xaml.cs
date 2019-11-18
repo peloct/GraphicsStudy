ccd067dc6f862ae45a4c0119a5b6947acfa1ed09
HyukHoLee <peloct@naver.com>
2019-11-18 17:19:06 +0900

    bind opengl (opentk)

--- GraphicsStudy/MainWindow.xaml.cs
+++ GraphicsStudy/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Diagnostics;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL4;
 
 namespace GraphicsStudy
 {
@@ -23,6 +15,30 @@ namespace GraphicsStudy
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void WindowsFormsHost_Initialized(object sender, EventArgs e)
+        {
+            Debug.WriteLine("WindowsFormsHost_Initialized");
+            glControl.MakeCurrent();
+        }
+
+        private void GLControl_Load(object sender, EventArgs e)
+        {
+            Debug.WriteLine("GLControl_Load");
+            GL.ClearColor(new Color4(0.631f, 0.6f, 0.227f, 1f));
+        }
+
+        private void GLControl_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            glControl.SwapBuffers();
+        }
+
+        private void GLControl_Resize(object sender, EventArgs e)
+        {
+            Debug.WriteLine(string.Format("GLControl_Resize : width = {0} height = {1}", glControl.Width, glControl.Height));
+            GL.Viewport(0, 0, glControl.Width, glControl.Height);
         }
     }
 }
